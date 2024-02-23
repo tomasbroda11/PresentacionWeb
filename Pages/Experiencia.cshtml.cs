4ed4045279b71930557c452dc0b28be8ca096d60
tomasbroda11 <tomasbroda13@gmail.com>
2024-02-23 17:46:54 -0300

    primer commit

---
+++ Pages/Experiencia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPresentacion.Pages
+{
+    public class ExperienciaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages(); // Configuración de las rutas de páginas Razor
+
+app.MapFallbackToPage("/Index");
+
+app.Run();
